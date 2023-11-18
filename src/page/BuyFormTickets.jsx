03f32a71cef60d7c6ba12f8298c2a52854fc3a50
GlebRuban <104936944+GlebRuban@users.noreply.github.com>
2023-11-18 23:36:11 +0300

    изменение стилей и формы

--- src/page/BuyFormTickets.jsx
+++ src/page/BuyFormTickets.jsx
@@ -3,6 +3,7 @@ import "../css/Form.css";
 import React, { useState } from 'react';
 
 export default function BuyFormTickets() {
+
   const [selectedSeat, setSelectedSeat] = useState(null);
   const handleSeatClick = (seat) => {
     setSelectedSeat(seat);
@@ -28,33 +29,58 @@ export default function BuyFormTickets() {
     }
     return seats;
   };
+  const [formData, setFormData] = useState({});
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const jsonString = JSON.stringify(formData);
+    saveJsonToFile(jsonString, 'data.json');
+  };
+
+  const saveJsonToFile = (jsonString, filename) => {
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+
+    link.href = url;
+    link.download = filename;
+    link.click();
+
+    URL.revokeObjectURL(url);
+  };
 
   return<>
   {/* Form Content */}
   <div className="login-box">
     <h2>Заказ билета</h2>
-    <form className="form_tickets">
-      <div>
-      <div className="user-box">
-        <input type="text" name="firstname"  pattern="^[A-Za-zА-Яа-яЁё\s]+$"/>
-        <label>Имя</label>
-      </div>
-      <div className="user-box">
-        <input type="text" name="surname" pattern="^[A-Za-zА-Яа-яЁё\s]+$"/>
-        <label>Фамилия</label>
-      </div>
-      <div className="user-box">
-        <input type="tel" name="tel" pattern="[+789][0-9]{11}" />
-        <label>Телефон</label>
-      </div>
+    <form className="form_tickets" onSubmit={handleSubmit}>
+      <div className="class">
+        <div className="user-box">
+          <input type="text" name="firstname"  pattern="^[A-Za-zА-Яа-яЁё\s]+$" onChange={handleChange} />
+          <label>Имя</label>
+        </div>
+        <div className="user-box">
+          <input type="text" name="surname" pattern="^[A-Za-zА-Яа-яЁё\s]+$" onChange={handleChange} />
+          <label>Фамилия</label>
+        </div>
+        <div className="user-box">
+          <input type="tel" name="tel" pattern="[+789][0-9]{11}" onChange={handleChange}/>
+          <label>Телефон</label>
+        </div>
       </div>
       <table className="App">
         <tbody>
-        <div className="selected-info">
+        <div className="selected-info" name="row and place" onChange={handleChange}>
         {selectedSeat && (
           <p className="Row">
             Выбрано место: Ряд 
-            <span className="Row_span">
+            <span className="Row_span" >
               {selectedSeat.split('-')[0]}
             </span>
             Место
@@ -67,13 +93,13 @@ export default function BuyFormTickets() {
         </tbody>
         <div className="seat-container">{renderSeats()}</div>
       </table>
-      <a href="#">
+      <button type="submit">
         <span></span>
         <span></span>
         <span></span>
         <span></span>
         Заказать
-      </a>
+      </button>
     </form>
   </div>
 </>    
