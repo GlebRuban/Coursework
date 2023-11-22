e790d33ac061de52a4b0dd216d840da8435ef366
GlebRuban <104936944+GlebRuban@users.noreply.github.com>
2023-11-22 23:07:21 +0300

    Нет исправлений

--- src/api/save-data/server.js
+++ src/api/save-data/server.js
@@ -1,11 +1,9 @@
 // const express = require('express');
 // const fs = require('fs');
-
 // const app = express();
 // const port = 3001;
 
 // app.use(express.json());
-
 // app.post('/api/save-data', (req, res) => {
 //   const data = req.body;
 //   fs.writeFile('./data.json', JSON.stringify(data), 'utf8', (err) => {
